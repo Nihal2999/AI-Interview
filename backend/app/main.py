3e14a6ba42a28a086c303ee2fa2d18c410fbf8bc
Nihal V <nihalvernekar990@gmail.com>
2026-03-22 14:06:52 +0530

    update: update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],     # Tighten this in production
+    allow_origins=["https://ai-interview-prod.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
